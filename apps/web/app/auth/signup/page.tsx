5d961a30a0f8c52a567f4b69449579a371f54365
Ivan Kolokoltsev <ikolokoltsev@gmail.com>
2025-05-22 01:09:52 +0200

    Created SignUpForm with sing up function

--- apps/web/app/auth/signup/page.tsx
+++ apps/web/app/auth/signup/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import SignUpForm from "./signUpForm";
 
 const SignUpPage = () => {
   return (
-    <div className="bg-white p-8 roubnded-lg shadow-md w-96 flex flex-col justify-center items-center">
+    <div className="bg-white p-8 rounded-lg shadow-md w-96 flex flex-col justify-center items-center">
       <h1 className="text-center text-2xl font-bold mb-4">Sign Up Page</h1>
-
+      <SignUpForm />
       <div className="flex justify-between text-sm">
         <p>Already have an account?</p>
         <Link className="underline" href="/auth/signup">
---
+++ apps/web/lib/type.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+export type FormState =
+  | {
+      error?: {
+        name: string;
+        email: string;
+        password: string[];
+      };
+      message?: string;
+    }
+  | undefined;
+
+export const SignUpFormSchema = z.object({
+  name: z.string().min(2, { message: "Name is required" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: "Password must contain at least one special character",
+    })
+    .trim(),
+});
