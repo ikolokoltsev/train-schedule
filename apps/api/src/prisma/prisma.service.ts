0cca20829632f6c2332cbc312642d69e13ae7350
Ivan Kolokoltsev <ikolokoltsev@gmail.com>
2025-05-21 21:18:00 +0200

    Updated prisma service with correct import path

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from 'prisma/generated/prisma-client-js';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit(): Promise<void> {
+  async onModuleInit() {
     await this.$connect();
   }
 }
