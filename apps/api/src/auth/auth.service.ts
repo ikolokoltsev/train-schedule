104cca32c424996cbc78445d5beb0b9bb7f4991a
Ivan Kolokoltsev <ikolokoltsev@gmail.com>
2025-05-21 21:21:57 +0200

    Updated auth api with register user endpoint

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { CreateUserDto } from '../user/dto/create-useer.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async registerUser(createUserDto: CreateUserDto) {
+    const user = await this.userService.findByEmail(createUserDto.email);
+    if (user) {
+      throw new ConflictException('User already exists');
+    }
+    return this.userService.create(createUserDto);
+  }
+}
