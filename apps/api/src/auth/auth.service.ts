862ebe108055929e09069589df4caf235866c8da
Ivan Kolokoltsev <ikolokoltsev@gmail.com>
2025-05-22 01:48:09 +0200

    Implemented local startegy and local user validation

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { verify } from 'argon2';
 import { UserService } from 'src/user/user.service';
 import { CreateUserDto } from '../user/dto/create-useer.dto';
 
@@ -12,5 +13,13 @@ export class AuthService {
       throw new ConflictException('User already exists');
     }
     return this.userService.create(createUserDto);
+  }
+  async validateLocalUSer(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+    if (!user) throw new UnauthorizedException('User not found');
+    if (!user.password) throw new UnauthorizedException('User password not set');
+    const isPasswordValid = await verify(user.password, password);
+    if (!isPasswordValid) throw new UnauthorizedException('Invalid password');
+    return { id: user.id, name: user.name };
   }
 }
