e029bc346d89dd2e18447c748e574894911efa08
Ivan Kolokoltsev <ikolokoltsev@gmail.com>
2025-05-22 03:19:37 +0200

    Prepared jwt auth guard

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from '../user/dto/create-useer.dto';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
 
 @Controller('auth')
@@ -15,5 +16,11 @@ export class AuthController {
   @Post('signin')
   loginUser(@Request() request: { user: { id: number; name: string } }) {
     return this.authService.login(request.user.id, request.user.name);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('protected')
+  getAll(@Request() request: { user: { id: number } }) {
+    return 'Now you can see this protected route. This is your user id: ' + request.user.id;
   }
 }
