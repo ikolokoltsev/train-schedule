7e9a91c2c19767ca368ff338f8282b88639a84ab
Ivan Kolokoltsev <ikolokoltsev@gmail.com>
2025-05-27 16:04:45 +0200

    Fixed env, and added railways.toml

--- apps/web/lib/actions.ts
+++ apps/web/lib/actions.ts
@@ -1,17 +1,15 @@
 "use server";
 
+import { BACKEND_URL } from "./constant";
 import { getSession } from "./session";
 
 export const getProfile = async () => {
   const session = await getSession();
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/protected`,
-    {
-      headers: {
-        authorization: `Bearer ${session?.accessToken}`,
-      },
-    }
-  );
+  const response = await fetch(`${BACKEND_URL}/auth/protected`, {
+    headers: {
+      authorization: `Bearer ${session?.accessToken}`,
+    },
+  });
   const result = await response.json();
   return result;
 };
